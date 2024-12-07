53e39c37a735481dedf3795439624e2419424147
Ivan <70844716+JohanPeper@users.noreply.github.com>
2024-12-07 17:25:43 +0300

    feat: ready, it turns out the first and last ones are not included

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.answer = null;
+    }
 
     setRange(min, max) {
-
+        this.min = (min == 0) ? min + 1 : min;
+        this.max = max - 1;
+        this.answer = Math.ceil((this.max + this.min) / 2);
     }
 
     guess() {
-
+        return this.answer;
     }
 
     lower() {
-
+        this.max = this.answer - 1;
+        this.answer = Math.ceil((this.max + this.min) / 2);
     }
-
     greater() {
-
+        this.min = this.answer + 1;
+        this.answer = Math.ceil((this.max + this.min) / 2);
     }
-}
 
+}
 module.exports = GuessingGame;
